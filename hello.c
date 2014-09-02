29cb972ec3d16cf8c71df0db9b15cb59ac5fb193
gjmilne <gjmilne@gmail.com>
2014-09-02 21:56:45 +1200

    playing with branch so modifying the code

--- hello.c
+++ hello.c
@@ -2,6 +2,6 @@
 
 int main(int argc, char *argv[])
 {
-	printf("Hello, World!\n");
+	printf("Hello, World, on a branch!\n");
 	return 0;
 }
--- hello.rb
+++ hello.rb
@@ -1,3 +1,3 @@
 # you need to use puts to get a newline, print doesn't add one
 
-puts "Hello, World!"
+puts "Hello, World, on a branch!"
